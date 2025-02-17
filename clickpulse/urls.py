0b4331874da5a24bf21884fdc5da94f91ca2da78
david <david@libply.com>
2025-02-17 16:03:05 +0200

    grafana URL

--- clickpulse/urls.py
+++ clickpulse/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/analytics/', include('analytics.urls')),
+    path('', lambda request: redirect('api/analytics/grafana/', permanent=True)),
 ]
